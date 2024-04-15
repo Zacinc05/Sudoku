701f4bb755bfeb739e9571224ea785d43d7b97e5
Zacinc05 <166748475+Zacinc05@users.noreply.github.com>
2024-04-14 20:56:47 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,5 @@
 python3 sudoku.py
+#i think we remove the 3 in python 3? pretty sure our pycharm doesnt run the 3 but is how the old system ran when they made the assignment
+'''In addition to the above classes, students will have a sudoku.py file, where the main function will be run. This
+file will contain code to create the different screens of the project (game start, game over, and game in
+progress), and will form a cohesive project together with the rest of the code'''
