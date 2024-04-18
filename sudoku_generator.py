ceb5a9e4bbf6e546344b4555fa2537889d69e0cc
Minh <77218183+Abe016@users.noreply.github.com>
2024-04-18 10:28:52 -0400

    small change to valid_in_box

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -97,10 +97,12 @@ class SudokuGenerator:
         col_start = (col_start // 3)*3
         for i in range(self.box_length): #checks the col 3 times
             for j in range(self.box_length): #checks the row 3 times
-                if self.board[col_start+i][row_start+j] == num:
+                # if self.board[col_start+i][row_start+j] == num:
+                if self.board[row_start + i][col_start + j] == num:
                     return False
         return True
     #Zac 4/16: i put some weird floor division and multiplication to make sure it actually starts at the box incase wrong number was put
+    #Minh 4/18: I was having errors with changing the screens when it was line 100 instead of 101
 
     '''
     Determines if it is valid to enter num at (row, col) in the board
@@ -233,50 +235,6 @@ class SudokuGenerator:
                 if self.board[sloty][slotx] != 0:
                     self.board[sloty][slotx] = 0
                     break
-
-class Board:
-    def __init__(self, width, height, screen, difficulty):
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.difficulty = difficulty
-	
-    '''Draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes. Draws every cell on this board'''
-    def draw(self):
-        pass
-    '''Marks the cell at (row, col) in the board as the current selected cell. Once a cell has been selected, the user can edit its value or sketched value'''
-    def select(self, row, col):
-        pass
-    def click(self, x, y):
-        pass
-
-    '''If a tuple of (x, y) coordinates is within the displayed board, this function returns a tuple of the (row, col) of the cell which was clicked. Otherwise, this function returns None.'''
-    def clear(self):
-        pass
-    '''Clears the value cell. Note that the user can only remove the cell values and sketched value that are
-    filled by themselves.'''
-    def sketch(self, value):
-        pass
-    '''Sets the sketched value of the current selected cell equal to user entered value. It will be displayed at the top left corner of the cell using the draw() function.'''
-    def place_number(self, value):
-        pass
-    '''Sets the value of the current selected cell equal to user entered value. Called when the user presses the Enter key.'''
-    def reset_to_original(self):
-        pass
-    '''Reset all cells in the board to their original values (0 if cleared, otherwise the corresponding digit).'''
-    def is_full(self):
-        pass
-    '''Returns a Boolean value indicating whether the board is full or not.'''
-    def update_board(self):
-        pass
-    '''Updates the underlying 2D board with the values in all cells.'''
-    def find_empty(self):
-        pass
-    '''Finds an empty cell and returns its row and col as a tuple (x, y).'''
-    def check_board(self):
-        pass
-    '''Check whether the Sudoku board is solved correctly.'''
-
 
     '''
     DO NOT CHANGE
